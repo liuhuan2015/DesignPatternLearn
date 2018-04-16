c8f603ab4e5e19b651ddd372003b49c1326d6761
liuhuan2015 <2210712252@qq.com>
2018-04-16 22:04:39 +0800

    Hook掉Context.startActivity(...),使得在每次调用这个方法之前都回输出一条日志.使用了反射,静态代理.

--- dynamic-proxy-hook/src/main/java/com/liuh/designpatternlearn/MainActivity.java
+++ dynamic-proxy-hook/src/main/java/com/liuh/designpatternlearn/MainActivity.java
@@ -1,7 +1,14 @@
 package com.liuh.designpatternlearn;
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.liuh.designpatternlearn.hook.HookHelper;
 
 import butterknife.ButterKnife;
 
@@ -10,8 +17,39 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        ButterKnife.bind(this);
+//        setContentView(R.layout.activity_main);
+//        ButterKnife.bind(this);
+        Button tv = new Button(this);
+        tv.setText("测试界面");
+
+        setContentView(tv);
+
+        tv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                intent.setData(Uri.parse("http://www.baidu.com"));
+
+                //注意这里是使用的ApplicationContext启动的Activity
+                //因为Activity对象的startActivity使用的并不是ContextImpl的mInstrumentation
+                //而是自己的mInstrumentation,如果你需要这样，可以自己Hook
+                //比较简单，直接替换这个Activity的此字段即可
+//                getApplicationContext().startActivity(intent);
+                startActivity(intent);
+            }
+        });
     }
 
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(newBase);
+
+        try {
+            //在这里进行Hook
+            HookHelper.attachContext();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
--- dynamic-proxy-hook/src/main/java/com/liuh/designpatternlearn/hook/EvilInstrumentation.java
+++ dynamic-proxy-hook/src/main/java/com/liuh/designpatternlearn/hook/EvilInstrumentation.java
@@ -1,6 +1,14 @@
 package com.liuh.designpatternlearn.hook;
 
+import android.app.Activity;
 import android.app.Instrumentation;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.util.Log;
+
+import java.lang.reflect.Method;
 
 /**
  * Created by huan on 2018/4/15.
@@ -13,7 +21,32 @@ public class EvilInstrumentation extends Instrumentation {
     //Activity中的原始的对象，保存起来
     Instrumentation mBase;
 
+    public EvilInstrumentation(Instrumentation mBase) {
+        this.mBase = mBase;
+    }
 
+    public ActivityResult execStartActivity(Context who, IBinder contextThread, IBinder token,
+                                            Activity target, Intent intent, int requestCode,
+                                            Bundle options) {
+        //Hook之前，xxx到此一游
+        Log.e(TAG, "\n执行了startActivity，参数如下：\n" + "who=" + who + ",contextThread=" + contextThread
+                + ",token=" + token + ",target=" + target + ",intent=" + intent + ",requestCode=" + requestCode
+                + ",options=" + options);
+        //开始调用原始的方法，调不调随你，但是如果不调用的话，所有的startActivity就会都失效。
+        //由于这个方法是隐藏的，因此需要使用反射调用；首先找到这个方法
+        try {
+            Method execStartActivity = Instrumentation.class.getDeclaredMethod(
+                    "execStartActivity",
+                    Context.class, IBinder.class, IBinder.class, Activity.class,
+                    Intent.class, int.class, Bundle.class);
+            return (ActivityResult) execStartActivity.invoke(mBase, who, contextThread, token, target,
+                    intent, requestCode, options);
 
+        } catch (Exception e) {
+            //被某个rom修改了，需要手动适配
+            e.printStackTrace();
+            throw new RuntimeException("do not support!!! pls adapt it");
+        }
+    }
 
 }
